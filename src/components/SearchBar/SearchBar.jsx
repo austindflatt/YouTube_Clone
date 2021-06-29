f64d2b9dd71fa01b997d4396a082c3b11973827d
Austin Flatt <austindflatt@gmail.com>
2021-06-29 14:12:57 -0400

    trying to get search function working

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { Input, Form, Button } from 'semantic-ui-react'
 
-const SearchBar = () => (
-    <Form action="/" method="get">
-        <input
-            type="text"
-            id="header-search"
-            placeholder="Search Youtube Videos"
-            name="s" 
-        />
-        <Button type="submit">Search</Button>
-    </Form>
-);
+class SearchBar extends React.Component {
+    state = {
+        searchTerm: ''
+    };
+    handleChange = (event) => this.setState({ searchTerm: event.target.value });
+    handleSubmit = (event) => {
+        const { searchTerm } = this.state;
+        const { onFormSubmit } = this.props;
+        onFormSubmit(searchTerm);
+        event.preventDefault();
+    };
+    render() {
+        return (
+                <Form onSubmit={this.handleSubmit}>
+                    <Input placeholder="Search Youtube Videos" onChange={this.handleChange} />
+                    <Button type="submit">Search</Button>
+                </Form>
+        );
+    }
+}
 
 export default SearchBar;
